ac370b4c02b96f4f5dbcf8ee2a54d63581ff5faf
topher2010 <ncrchris@gmail.com>
2019-03-01 15:24:18 -0700

    Update MilestoneProject.py

--- MilestoneProject.py
+++ MilestoneProject.py
@@ -1,4 +1,4 @@
-
+import random
 
 def display_board(board):
     print(' ' + board[1] + ' ' + ' | ' + ' ' + board[2] + ' ' + ' | ' + ' ' + board[3] + ' ')
@@ -30,3 +30,20 @@ def win_check(board, marker):
            (board[1] == marker and board[5] == marker and board[9] == marker) or #diagonal left to right
            (board[3] == marker and board[5] == marker and board[7] == marker)) #diagonal right to left
 
+def choose_first():
+    if random.randint(0,1) == 0:
+        return 'Player 2'
+    else:
+        return 'Player 1'
+    
+def space_check(board, position):
+    return board[position] == ' '
+
+def full_board_check(board):
+    for i in range(1,10):
+        if space-check(board, i):
+            return False
+        return True
+    
+def player_choice(board):
+    
