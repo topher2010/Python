dda82fd03224374a7421e19991bc7072f33bb275
topher2010 <ncrchris@gmail.com>
2019-03-05 15:09:31 -0700

    Update MilestoneProject.py

--- MilestoneProject.py
+++ MilestoneProject.py
@@ -1,49 +1,25 @@
-import random
+
+
+board = [' ']*10
 
 def display_board(board):
     print(' ' + board[1] + ' ' + ' | ' + ' ' + board[2] + ' ' + ' | ' + ' ' + board[3] + ' ')
+    print('---------------')
     print(' ' + board[4] + ' ' + ' | ' + ' ' + board[5] + ' ' + ' | ' + ' ' + board[6] + ' ')
+    print('---------------')
     print(' ' + board[7] + ' ' + ' | ' + ' ' + board[8] + ' ' + ' | ' + ' ' + board[9] + ' ')
 
 def player_input():
-    marker = ' '
-
-    while not (marker == 'X' or marker == 'O'):
-        marker == input("Player 1: Choose X or O ").upper()
-
-    if marker == 'X':
-        return ('X', 'O')
+    marker = ''
+    
+    while marker != 'X' and marker != 'O':
+        marker = input('Player 1, choose X or O: ')
+    
+    player1 = marker
+    
+    if player1 == 'X':
+        player2 = 'O'
     else:
-        return ('0', 'X')
-
-def place_marker(board, marker, position):
-    board[position] = marker
-    
-def win_check(board, marker):
-    
-    return ((board[1] == marker and board[2] == marker and board[3] == marker) or #across the top
-           (board[4] == marker and board[5] == marker and board[6] == marker) or #across middle
-           (board[7] == marker and board[8] == marker and board[9] == marker) or #across bottom
-           (board[1] == marker and board[4] == marker and board[7] == marker) or #down left
-           (board[2] == marker and board[5] == marker and board[8] == marker) or #down center
-           (board[3] == marker and board[6] == marker and board[9] == marker) or #down right
-           (board[1] == marker and board[5] == marker and board[9] == marker) or #diagonal left to right
-           (board[3] == marker and board[5] == marker and board[7] == marker)) #diagonal right to left
-
-def choose_first():
-    if random.randint(0,1) == 0:
-        return 'Player 2'
-    else:
-        return 'Player 1'
-    
-def space_check(board, position):
-    return board[position] == ' '
-
-def full_board_check(board):
-    for i in range(1,10):
-        if space-check(board, i):
-            return False
-        return True
-    
-def player_choice(board):
-    
+        player2 = 'X'
+        
+    return (player1,player2)
