c546dc38cf5f33aa19c5813a274f2298968e5a01
topher2010 <ncrchris@gmail.com>
2020-05-18 20:11:24 -0700

    Updated code
    Updated the code to include asking where you want the files moved to, as well as worked on the if/elif tree some more

--- SortFiles.py
+++ SortFiles.py
@@ -1,32 +1,30 @@
 import os, shutil
 
 src = input("Input the directory to work in: ")
-video_file = [".mkv", ".mp4", ".avi"]
+dest = input("Input where you want the files moved to: ")
+video_type = [".mkv", ".mp4", ".avi"]
 doc_type = [".docx", ".xlsx", ".pdf", ".txt", ".csv"]
+exec_type = [".exe"]
+image_type = [".jpg"]
 
 for file in os.listdir(src):
-    thing = os.path.splitext(file)
+    extension = os.path.splitext(file)
 
-    if (thing[1] in video_file):
-        dst = os.path.join(src, "Video Files")
-        if not os.path.exists(dst):
-            os.makedirs(dst)
-        shutil.move(os.path.join(src, file), dst)
+    if (extension[1] in video_type):
+        dst = os.path.join(dest, "Video Files")
 
-    elif thing[1] == ".exe":
-        dst = os.path.join(src, "Installers")
-        if not os.path.exists(dst):
-            os.makedirs(dst)
-        shutil.move(os.path.join(src, file), dst)
+    elif (extension[1] in exec_type):
+        dst = os.path.join(dest, "Installers")
 
-    elif (thing[1] in doc_type):
-        dst = os.path.join(src, "Documents")
-        if not os.path.exists(dst):
-            os.makedirs(dst)
-        shutil.move(os.path.join(src, file), dst)
+    elif (extension[1] in doc_type):
+        dst = os.path.join(dest, "Documents")
 
-    elif thing[1] == ".jpg":
-        dst = os.path.join(src, "Images")
-        if not os.path.exists(dst):
-            os.makedirs(dst)
-        shutil.move(os.path.join(src, file), dst)
+    elif (extension[1] in image_type):
+        dst = os.path.join(dest, "Images")
+
+    else:
+        continue
+
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    shutil.move(os.path.join(src, file), dst)
